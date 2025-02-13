00cb1347e1ad0c425f02d2412f531ecc4b2afff5
zawhtetnaing10 <zawgyi.gog@gmail.com>
2025-02-13 09:19:13 +0630

    tests added for textnode

---
+++ src/test_testnode.py
@@ -0,0 +1,37 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_different_text_type(self):
+        node = TextNode("This is a text node", TextType.ITALIC_TEXT)
+        node2 = TextNode("This is a text node", TextType.NORMAL_TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_none_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC_TEXT)
+        node2 = TextNode("This is a text node",
+                         TextType.NORMAL_TEXT, "https://www.boodev.com")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_different_text(self):
+        node = TextNode("This is an italic text node", TextType.NORMAL_TEXT)
+        node2 = TextNode("This is a text node", TextType.NORMAL_TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_different_urls(self):
+        node = TextNode("This is a text node",
+                        TextType.NORMAL_TEXT, "https://www.bootdev.com")
+        node2 = TextNode("This is a text node",
+                         TextType.NORMAL_TEXT, "https://www.google.com")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -11,13 +11,13 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text: str, text_type: TextType, url: str):
+    def __init__(self, text: str, text_type: TextType, url: str = None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        return self.text == other.text and self.text_type == other.tex_type and self.url == other.url
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self):
         return f"TextNode({self.text},{self.text_type.value},{self.url})"
